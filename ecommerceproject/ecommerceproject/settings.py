8ed83e4b072416eb9a0b805166cc557b41a05b8d
DipaliNeosoft <boradedipali81@gmail.com>
2023-01-22 18:25:12 +0530

    product gallery

--- ecommerceproject/ecommerceproject/settings.py
+++ ecommerceproject/ecommerceproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0!2l=^m_h5f10nxev)q_ma@x&o58c9w@^m@_s=z=4dyuc1gt*g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +55,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'account/login'
+
 
 ROOT_URLCONF = 'ecommerceproject.urls'
 
@@ -147,8 +155,8 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER  = 'boradedipali81@gmail.com'
-EMAIL_HOST_PASSWORD = 'duucipkhecphxmgk'
-EMAIL_USE_TLS = True 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER  = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool) 
--- ecommerceproject/store/views.py
+++ ecommerceproject/store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from carts.models import CartItem
 from carts.views import _cart_id
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 from orders.models import OrderProduct
 from django.db.models import Q
 from category.models import Category
@@ -50,11 +50,14 @@ def product_detail(request,category_slug,product_slug):
     #get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id,status=True)
 
+    # get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
     context ={
         'single_product':single_product,
         'in_cart':in_cart,
         'orderproduct':orderproduct,
         'reviews':reviews,
+        'product_gallery':product_gallery,
     }
     return render(request,'store/product_detail.html',context)
 
